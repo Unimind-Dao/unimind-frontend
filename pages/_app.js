c532cf0186b718d8b9e098f29acbe9e094c23baf
adiusz <leeszczuu@gmail.com>
2022-08-02 17:51:51 +0200

    fix the theme issue - purple color thing + small changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,55 +1,47 @@
 import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
-import { ThemeProvider } from "styled-components";
 import { darkTheme, getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig, } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-import Toaster from "../components/Toaster/Toaster"
-import { useTheme } from "@mui/material";
-import '@rainbow-me/rainbowkit/styles.css';
-import "../index.css"
-import "../styles/fonts.css";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import Toaster from "../components/Toaster/Toaster";
 import { COLORS } from "../theme/theme";
+import "@rainbow-me/rainbowkit/styles.css";
+import "../index.css";
+import "../styles/fonts.css";
 
 const { chains, provider } = configureChains(
   [chain.rinkeby],
-  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()],
 );
 
 const { connectors } = getDefaultWallets({
   appName: "My RainbowKit App",
-  chains
+  chains,
 });
 
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  provider
+  provider,
 });
 
 const chainId = ChainId.Rinkeby;
 
 function UnimindDao({ Component, pageProps }) {
-
-  const theme = useTheme();
-
   return (
-    <ThemeProvider theme={theme}>
-      <ThirdwebProvider desiredChainId={chainId}>
-        <WagmiConfig client={wagmiClient}>
-          <RainbowKitProvider chains={chains} theme={darkTheme({
-            accentColor: COLORS.primary,
-            accentColorForeground: 'black',
-            borderRadius: 'small',
-            fontStack: 'system',
-          })}>
-            <Component {...pageProps} />
-            <Toaster />
-          </RainbowKitProvider>
-        </WagmiConfig>
-      </ThirdwebProvider>
-    </ThemeProvider>
-  )
+    <ThirdwebProvider desiredChainId={chainId}>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={darkTheme({
+          accentColor: COLORS.primary,
+          accentColorForeground: "black",
+          borderRadius: "small",
+        })}>
+          <Component {...pageProps} />
+          <Toaster />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ThirdwebProvider>
+  );
 }
 
 export default UnimindDao;
