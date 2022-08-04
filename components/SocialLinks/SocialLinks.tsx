690bcf76804c6e1e854c8f76206a84b985c414f1
Tomasz Kotowski <t.kotowski.2@gmail.com>
2022-08-04 13:05:51 +0200

    add face and tiktok links

--- components/SocialLinks/SocialLinks.tsx
+++ components/SocialLinks/SocialLinks.tsx
@@ -1,36 +1,20 @@
 import React from "react";
 
-import YouTubeIcon from "@mui/icons-material/YouTube";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import { Grid, GridProps } from "@mui/material";
 
-import DiscordIcon from "../svg/Discord";
 import { CustomLink } from "../CustomLink";
+import { data } from "./data";
 
 export const SocialLinks = (props: GridProps) => {
   return (
     <Grid container columnSpacing={4} {...props}>
-      <Grid item>
-        <CustomLink href={process.env.NEXT_PUBLIC_TWITTER_LINK} target="_blank">
-          <TwitterIcon sx={{ color: "white" }} />
-        </CustomLink>
-      </Grid>
-      <Grid item>
-        <CustomLink
-          href={process.env.NEXT_PUBLIC_DISCORD_INVITE_LINK}
-          target="_blank"
-        >
-          <DiscordIcon />
-        </CustomLink>
-      </Grid>
-      <Grid item>
-        <CustomLink
-          href={process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_LINK}
-          target="_blank"
-        >
-          <YouTubeIcon sx={{ color: "white" }} />
-        </CustomLink>
-      </Grid>
+      {data.map(({ id, href, icon }) => (
+        <Grid item key={id}>
+          <CustomLink href={href} target="_blank">
+            {icon}
+          </CustomLink>
+        </Grid>
+      ))}
     </Grid>
   );
 };
