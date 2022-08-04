ba09ac783d55ed3c4ed0209f39760e8957dab29c
Tomasz Kotowski <t.kotowski.2@gmail.com>
2022-08-04 11:25:44 +0200

    Small changes to lintstagedrc and package scripts

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,18 +5,11 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`;
 
-const formatCommand = (filenames) => `yarn prettier ${filenames.join(" ")}`;
-const tsCheckCommand = () => "yarn tsc --noEmit";
+const formatCommand = (filenames) => `npx prettier ${filenames.join(" ")}`;
+const tsCheckCommand = () => "npx tsc --noEmit";
 
 module.exports = {
-  // this will check Typescript files
   "**/*.(ts|tsx)": tsCheckCommand,
-  //   "*.{js,jsx,ts,tsx}": () => "yarn format",
-  //   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
-
-  // This will lint and format TypeScript and                                             //JavaScript files
   "*.{js,jsx,ts,tsx}": [buildEslintCommand, formatCommand],
-
-  // this will Format MarkDown and JSON
   "**/*.(md|json)": formatCommand,
 };
