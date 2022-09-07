675d83bf36858222e60cb19c403f86690ef87ac7
Tomasz Kotowski <t.kotowski.2@gmail.com>
2022-09-07 21:43:20 +0200

    add cache support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import { styled } from "@mui/material";
+import { GetServerSideProps, GetStaticProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-import { About, Footer,Header, Hero, JoinUnimindSection, OurMission, Partners, Team,WhatDrivesUs } from "../components";
+import {
+  About,
+  Footer,
+  Header,
+  Hero,
+  JoinUnimindSection,
+  OurMission,
+  Partners,
+  Team,
+  WhatDrivesUs,
+} from "../components";
 import { sectionName } from "../data/constants";
 
 function App() {
@@ -41,17 +52,44 @@ const SectionWithPadding = styled("section")`
 `;
 
 const StyledDivider = styled("div")`
-  background-image: linear-gradient(to right, #252525, #2e2e2e, #383838, #424242, #4c4c4c, #4c4c4c, #4c4c4c, #4c4c4c, #424242, #383838, #2e2e2e, #252525);
+  background-image: linear-gradient(
+    to right,
+    #252525,
+    #2e2e2e,
+    #383838,
+    #424242,
+    #4c4c4c,
+    #4c4c4c,
+    #4c4c4c,
+    #4c4c4c,
+    #424242,
+    #383838,
+    #2e2e2e,
+    #252525
+  );
   height: 2px;
   width: 100%;
 `;
 
-export async function getStaticProps({ locale = "" }) {
+export const getServerSideProps: GetServerSideProps = async ({
+  locale = "",
+  res,
+}) => {
+  // Cache the content of this page for 12 hrs
+  // After 12 hrs, the content will continue to be served
+  // for a grace period of 60 seconds as new data is fetched
+  // Then, the CDN will store a fresh copy for the next user :-)
+
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=43200, stale-while-revalidate=60"
+  );
+
   return {
     props: {
       ...(await serverSideTranslations(locale)),
     },
   };
-}
+};
 
 export default App;
